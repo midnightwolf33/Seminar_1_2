61d847696683d3f9e843b3e85c6109363c1b2e72
midnight33 <gra33@bk.ru>
2023-02-28 20:18:45 +0300

    Задача 2 не поддается

--- Program.cs
+++ Program.cs
@@ -4,21 +4,18 @@
     a = Convert.ToInt32(Console.ReadLine());
     b = Convert.ToInt32(Console.ReadLine());
     c = Convert.ToInt32(Console.ReadLine());
-    if (a > b)
+    if (b < a > c);
     {
         Console.WriteLine("{0} наибольшее число", a);
     }
-    else
+    if (a < b > c);
     {
-        Console.WriteLine("{0} наибольшее число ", b);
-        if (b > c)
-        {
-            Console.WriteLine("{0} наибольшее число", b);
-        }
-        else
-        {
-            Console.WriteLine("{0} наибольшее число", c);
-        }
+        Console.WriteLine("{0} наибольшее число", b);
+    }
+    if (a < c > b);
+    {
+        Console.WriteLine("{0} наибольшее число", c);
+   
     }
     Console.ReadLine();
 }
